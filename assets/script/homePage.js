86238fa15b67d6a5a17b61b3f906ac9b79e92243
Atefeh <at.bakhshi1373@gmail.com>
2022-12-27 11:51:53 +0330

    feat: go to filter and product page when selected specific part

---
+++ assets/script/homePage.js
@@ -0,0 +1,52 @@
+import { addToDom } from "../modules/addToDom.js";
+const API_URL = "http://localhost:3000";
+const $ = document;
+
+//========= Dom variables =========
+const userNameInfo = $.querySelector("#user-name-info");
+const companyLogos = $.querySelector("#companies-logo");
+const productsContainer = $.querySelector("#products");
+const mostPopularButtons = $.querySelector("#header-buttons");
+
+//========= functions =========
+const readProduct = async () => {
+  try {
+    const res = await fetch(`${API_URL}/products`);
+    const data = await res.json();
+    addToDom(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//========= events =========
+// show user name on top of home page
+let userInfo = JSON.parse(localStorage.getItem("user"));
+userNameInfo.innerText = userInfo.name;
+
+// read product from API
+readProduct();
+
+// nav to product page
+productsContainer.addEventListener("click", (e) => {
+  const selectedProduct = e.target.closest(".product");
+  const id = selectedProduct.id;
+  const quantity = selectedProduct.children[2].innerText;
+  if (Number(quantity) !== 0) {
+    window.location.href = "productPage2.html?id=" + id;
+  } else {
+    window.location.href = "productPage.html?id=" + id;
+  }
+});
+
+// filter by company name
+companyLogos.addEventListener("click", (e) => {
+  const companyName = e.target.closest(".logo-name").children[1].innerText;
+  window.location.href = `homeFilter.html?model=${companyName}`;
+});
+
+// filter by header buttons
+mostPopularButtons.addEventListener("click", (e) => {
+  const companyName = e.target.innerText;
+  window.location.href = `homeMostPopularFilter.html?model=${companyName}`;
+});
