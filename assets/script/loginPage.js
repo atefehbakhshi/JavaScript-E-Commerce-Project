a4008f4e0c4671edd7ee5a9fd93ea837a0fc4edd
Atefeh <at.bakhshi1373@gmail.com>
2022-12-27 07:51:49 +0330

    feat: user login

--- assets/script/loginPage.js
+++ assets/script/loginPage.js
@@ -1,5 +1,5 @@
 const $ = document;
-
+// variables
 const boardingPageBtn = $.querySelector("#back-to-boarding-page");
 
 const form = $.querySelector("#login-form");
@@ -7,6 +7,7 @@ const userNameContainer = $.querySelector("#user-name-container");
 const passwordContainer = $.querySelector("#password-container");
 const userName = $.querySelector("#user-name");
 const userPass = $.querySelector("#user-pass");
+const visibility = $.querySelector("#visibility-off");
 const rememberMe = $.querySelector("#remember-me");
 
 const loginBtn = $.querySelector("#sign-in");
@@ -16,10 +17,17 @@ let user = {
   pass: "",
   remember: false,
 };
-
-boardingPageBtn.addEventListener('click',()=>{
-    window.location.href='onboardingpage3.html'
-})
+// functions
+const signIn = (userObject) => {
+  loginBtn.addEventListener("click", () => {
+    localStorage.setItem("user", JSON.stringify(userObject));
+    window.location.href = "homePage.html";
+  });
+};
+// events
+boardingPageBtn.addEventListener("click", () => {
+  window.location.href = "onboardingpage3.html";
+});
 
 form.addEventListener("keyup", (e) => {
   const targetId = e.target.id;
@@ -35,6 +43,17 @@ form.addEventListener("keyup", (e) => {
   user.pass = userPass.value;
   if (user.name !== "" && user.pass !== "") {
     loginBtn.style.opacity = 1;
+    signIn(user);
+  }
+});
+
+visibility.addEventListener("click", () => {
+  if (userPass.type === "password") {
+    userPass.type = "text";
+    visibility.innerText = "visibility";
+  } else {
+    userPass.type = "password";
+    visibility.innerText = "visibility_off";
   }
 });
 
@@ -42,13 +61,10 @@ rememberMe.addEventListener("click", (e) => {
   if (e.target.checked) {
     userNameContainer.classList.remove("border");
     passwordContainer.classList.remove("border");
-    user.remember=true;
-  }else{
+    user.remember = true;
+  } else {
     passwordContainer.classList.add("border");
-    user.remember=false;
+    user.remember = false;
   }
+  signIn(user);
 });
-
-loginBtn.addEventListener('click',()=>{
-    
-})
--- assets/script/onboardingPage1.js
+++ assets/script/onboardingPage1.js
@@ -1,2 +1,10 @@
 import { switchPage } from "../modules/switchPage.js";
+
+const user = JSON.parse(localStorage.getItem("user"));
+if (user !== null && user.remember) {
+  setTimeout(() => {
+    window.location.href = "homepage.html";
+  }, 2000);
+}
+
 switchPage(2);
